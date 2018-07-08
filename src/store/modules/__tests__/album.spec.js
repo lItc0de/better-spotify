7c48b928f05787f135bf946837eecb9e69ab0055
Basti <litc0de@posteo.org>
2018-07-08 12:01:01 +0200

    add album tracks and albums to store

--- src/store/modules/__tests__/album.spec.js
+++ src/store/modules/__tests__/album.spec.js
@@ -36,4 +36,72 @@ describe('player module', () => {
       expect(commit).not.toHaveBeenCalled();
     });
   });
+
+  describe('GET album tracks', () => {
+    it('has a default state', () => {
+      expect(album.state.currentTracks).toEqual([]);
+    });
+
+    it('sets the state', () => {
+      const currentTracks = ['foo'];
+      const state = { currentTracks: [] };
+      album.mutations.setCurrentTracks(state, currentTracks);
+      expect(state.currentTracks).toEqual(currentTracks);
+    });
+
+    it('commits the fetched items', async () => {
+      const commit = jest.fn();
+      const id = 'testId';
+      const limit = 20;
+      const offset = 2;
+      const currentTracks = ['foo'];
+      client.fetchFromSpotify.mockReturnValueOnce(currentTracks);
+      await album.actions.fetchAlbumTracks({ commit }, id, limit, offset);
+      expect(client.fetchFromSpotify).toHaveBeenCalledWith(`/albums/${id}/tracks?limit=${limit}&offset=${offset}`);
+      expect(commit).toHaveBeenCalledWith('setCurrentTracks', currentTracks);
+    });
+
+    it('does not call the commit method when there is a fetch error', async () => {
+      const commit = jest.fn();
+      await album.actions.fetchAlbumTracks({ commit });
+      expect(commit).not.toHaveBeenCalled();
+    });
+
+    it('sets default offset and limit', async () => {
+      const commit = jest.fn();
+      const id = 'testId';
+      await album.actions.fetchAlbumTracks({ commit }, id);
+      expect(client.fetchFromSpotify).toHaveBeenCalledWith(`/albums/${id}/tracks?limit=100&offset=0`);
+    });
+  });
+
+  describe('GET albums', () => {
+    it('has a default state', () => {
+      expect(album.state.albums).toEqual([]);
+    });
+
+    it('sets the state', () => {
+      const albums = ['foo'];
+      const state = { albums: [] };
+      album.mutations.setAlbums(state, albums);
+      expect(state.albums).toEqual(albums);
+    });
+
+    it('commits the fetched items', async () => {
+      const commit = jest.fn();
+      const ids = 'id1,id2,id3';
+      const idsArr = ids.split(',');
+      const albums = ['foo'];
+      client.fetchFromSpotify.mockReturnValueOnce(albums);
+      await album.actions.fetchAlbums({ commit }, idsArr);
+      expect(client.fetchFromSpotify).toHaveBeenCalledWith(`/albums?ids=${ids}`);
+      expect(commit).toHaveBeenCalledWith('setAlbums', albums);
+    });
+
+    it('does not call the commit method when there is a fetch error', async () => {
+      const commit = jest.fn();
+      await album.actions.fetchAlbums({ commit });
+      expect(commit).not.toHaveBeenCalled();
+    });
+  });
 });
--- src/store/modules/album.js
+++ src/store/modules/album.js
@@ -3,12 +3,22 @@ import { fetchFromSpotify } from '@/utils/fetchFromSpotify';
 export default {
   state: {
     current: {},
+    currentTracks: [],
+    albums: [],
   },
 
   /* eslint-disable no-param-reassign */
   mutations: {
     setCurrent(state, current) {
       state.current = current;
+    },
+
+    setCurrentTracks(state, currentTracks) {
+      state.currentTracks = currentTracks;
+    },
+
+    setAlbums(state, albums) {
+      state.albums = albums;
     },
   },
   /* eslint-enable no-param-reassign */
@@ -18,6 +28,18 @@ export default {
       const res = await fetchFromSpotify(`/albums/${id}`);
       if (!res) return;
       commit('setCurrent', res);
+    },
+
+    async fetchAlbumTracks({ commit }, id, limit = 100, offset = 0) {
+      const res = await fetchFromSpotify(`/albums/${id}/tracks?limit=${limit}&offset=${offset}`);
+      if (!res) return;
+      commit('setCurrentTracks', res);
+    },
+
+    async fetchAlbums({ commit }, ids) {
+      const res = await fetchFromSpotify(`/albums?ids=${ids}`);
+      if (!res) return;
+      commit('setAlbums', res);
     },
   },
 };
