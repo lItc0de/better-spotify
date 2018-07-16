03b206f022a27ac1a426e24d5d2f4739a26bf174
Basti <litc0de@posteo.org>
2018-07-16 22:46:28 +0200

    add vuex-router-sync

--- src/main.js
+++ src/main.js
@@ -1,13 +1,19 @@
 import Vue from 'vue';
+import { sync } from 'vuex-router-sync';
 import App from './App.vue';
-import router from './router';
+import router from './routes';
 import store from './store';
 import './registerServiceWorker';
 
 Vue.config.productionTip = false;
 
+const unsync = sync(store, router);
+
 new Vue({
   router,
   store,
   render: h => h(App),
+  beforeDestroy() {
+    unsync();
+  },
 }).$mount('#app');
