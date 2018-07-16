9cd42be492e8403df5c4749f7c4d29af675e7a99
Basti <litc0de@posteo.org>
2018-07-16 22:58:38 +0200

    add snapshot test for app component

---
+++ src/__tests__/App.spec.js
@@ -0,0 +1,22 @@
+import { shallowMount, createLocalVue } from '@vue/test-utils';
+import VueRouter from 'vue-router';
+import App from '@/App.vue';
+
+const localVue = createLocalVue();
+localVue.use(VueRouter);
+const router = new VueRouter();
+
+let wrapper;
+
+describe('App', () => {
+  beforeEach(() => {
+    wrapper = shallowMount(App, {
+      localVue,
+      router,
+    });
+  });
+
+  it('matches the snapshot', () => {
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+});
