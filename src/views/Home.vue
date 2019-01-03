687400c5b01b5b2032fee41070eb8b898d08a66a
litc0de <litc0de@posteo.org>
2019-01-03 09:00:00 +0100

    use layout in home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,19 @@
 <template>
-  <x-container>
-    <h1>Better Spotify</h1>
-  </x-container>
+  <layout>
+    <x-container>
+      <h1>Better Spotify</h1>
+    </x-container>
+  </layout>
 </template>
 
 <script>
+import Layout from '@/components/Layout.vue';
+
 export default {
   name: 'Home',
+
+  components: {
+    Layout,
+  },
 };
 </script>
