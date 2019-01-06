fbe08c793ef26b317026358cbc8c4741ccc1a8cb
litc0de <litc0de@posteo.org>
2019-01-06 22:57:21 +0100

    show an users playlists

--- src/features/playlists/views/Playlists.vue
+++ src/features/playlists/views/Playlists.vue
@@ -1,17 +1,37 @@
 <template>
-  <x-layout id="app">
-    <x-container>Playlists</x-container>
-  </x-layout>
+  <layout>
+    <x-layout id="app">
+      <x-container>
+        <x-grid-list>
+          <div v-for="playlist in items" :key="playlist.id">
+            <x-img :src="playlist.images[0].url">
+            </x-img>
+            <x-text slim>{{ playlist.name }}</x-text>
+            <x-text color="colorSecondary" slim>{{ playlist.owner.display_name }}</x-text>
+          </div>
+        </x-grid-list>
+      </x-container>
+    </x-layout>
+  </layout>
 </template>
 
 <script>
-import { mapActions } from 'vuex';
+import { mapActions, mapGetters } from 'vuex';
+import Layout from '@/components/Layout.vue';
 
 export default {
   name: 'Playlists',
 
+  components: {
+    Layout,
+  },
+
   methods: {
     ...mapActions('playlists', ['fetchList']),
+  },
+
+  computed: {
+    ...mapGetters('playlists', ['items']),
   },
 
   watch: {
