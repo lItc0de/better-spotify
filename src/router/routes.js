c0271ed2bc5cf43a2e45eadbcc07eeedc69a6387
litc0de <litc0de@posteo.org>
2019-03-21 19:37:56 +0100

    add webpack chunk names for lazy routes

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,21 +2,21 @@ export default [
   {
     path: '/',
     name: 'Home',
-    component: () => import('@/views/Home.vue'),
+    component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue'),
     meta: { auth: true },
   },
 
   {
     path: '/playlists',
     name: 'Playlists',
-    component: () => import('@/playlists/Index.vue'),
+    component: () => import(/* webpackChunkName: "playlists" */ '@/playlists/Index.vue'),
     meta: { auth: true },
   },
 
   {
     path: '/login',
     name: 'Login',
-    component: () => import('@/views/Login.vue'),
+    component: () => import(/* webpackChunkName: "login" */ '@/views/Login.vue'),
     meta: { auth: true },
   },
 ];
