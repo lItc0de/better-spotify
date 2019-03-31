a648ba69f68547fb648e545a6d10483fbe5287c2
litc0de <litc0de@posteo.org>
2019-03-31 14:06:54 +0200

    show playback state on controls

--- src/player/Controls.test.js
+++ src/player/Controls.test.js
@@ -11,6 +11,11 @@ localVue.use(Vuex);
 describe('Player Controls', () => {
   let store;
   let wrapper;
+  let shuffleBtn;
+  let previousBtn;
+  let playBtn;
+  let nextBtn;
+  let repeatBtn;
 
   beforeEach(() => {
     store = new Vuex.Store(clonedeep(storeConfig));
@@ -18,13 +23,41 @@ describe('Player Controls', () => {
       localVue,
       store,
     });
+
+    shuffleBtn = wrapper.find('[data-test="shuffle"]');
+    previousBtn = wrapper.find('[data-test="previous"]');
+    playBtn = wrapper.find('[data-test="play"]');
+    nextBtn = wrapper.find('[data-test="next"]');
+    repeatBtn = wrapper.find('[data-test="repeat"]');
+  });
+
+  describe('control buttons state', () => {
+    it('shows correct state', () => {
+      const playback = {
+        progress_ms: 1234,
+        is_playing: true,
+        item: {},
+        shuffle_state: true,
+        repeat_state: 'context',
+      };
+
+      expect(shuffleBtn.text()).toEqual('no_shuffle');
+      expect(previousBtn.text()).toEqual('previous');
+      expect(playBtn.text()).toEqual('play');
+      expect(nextBtn.text()).toEqual('next');
+      expect(repeatBtn.text()).toEqual('off');
+
+      store.commit('player/setPlayback', playback);
+
+      expect(shuffleBtn.text()).toEqual('shuffle');
+      expect(playBtn.text()).toEqual('pause');
+      expect(repeatBtn.text()).toEqual('context');
+    });
   });
 
   describe('Play/Pause button', () => {
     it('triggers play if playback is paused', () => {
-      const playBtn = wrapper.find('[data-test="play"]');
       store.dispatch = jest.fn();
-
       playBtn.trigger('click');
 
       expect(store.dispatch).toHaveBeenCalledWith('player/play', undefined);
--- src/player/Controls.vue
+++ src/player/Controls.vue
@@ -1,18 +1,24 @@
 <template>
   <x-container row>
-    <button data-test="shuffle" icon="shuffle">shuffle</button>
+    <button data-test="shuffle" icon="shuffle">{{ shuffle ? 'shuffle' : 'no_shuffle' }}</button>
     <button data-test="previous" icon="previous">previous</button>
-    <button data-test="play" icon="play" @click="handlePlay">play</button>
+    <button data-test="play" icon="play" @click="handlePlay">
+      {{ playing ? 'pause' : 'play' }}
+    </button>
     <button data-test="next" icon="next">next</button>
-    <button data-test="repeat" icon="repeat">repeat</button>
+    <button data-test="repeat" icon="repeat">{{ repeat }}</button>
   </x-container>
 </template>
 
 <script>
-import { mapActions } from 'vuex';
+import { mapActions, mapState } from 'vuex';
 
 export default {
   name: 'Controls',
+
+  computed: {
+    ...mapState('player', ['playing', 'progress', 'shuffle', 'repeat', 'track']),
+  },
 
   methods: {
     ...mapActions('player', ['play']),
